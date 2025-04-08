da2f38c04878f1d1d078d555dab98ee3447ea088
Manan Jariwala <mananjariwala01@gmail.com>
2025-04-08 17:03:19 +0530

    Refactor website layout and styles: Update CSS for improved responsiveness and aesthetics; restructure HTML for better organization and clarity.
    Added Stock Data of All the comapnies for over 20 years or the companies whole history.
    
    Need to change the stock list page where the list of stock is not appearing in appropriate styling.

--- main.py
+++ main.py
@@ -36,6 +36,29 @@ def add_company_details_to_csv():
             processed_file = processor.save_processed_data()
             print(f"Processed stock data saved to: {processed_file}")
 
+def add_specific_comapnies():
+    """Add company details to a CSV file."""
+    with open('companies.json', 'r') as file:
+        companies_details = json.load(file)
+
+        for i in range(20, 25):
+            # Initialize API objects
+            stock = stock_api.StockData(stock_symbol=companies_details[i]['symbol'], company_name=companies_details[i]['name'])
+            
+            # Ensure the output directory exists
+            ensure_directory_exists("website/images")
+
+            stock_data = stock.fetch_stock_data_json()
+            
+            # Process the stock data
+            processor = DataProcessor()
+            csv_file = processor.dict_to_csv(stock_data)
+            stock_df, ticker = processor.process_csv_data(csv_file)
+            
+            # Save the processed data
+            processed_file = processor.save_processed_data()
+            print(f"Processed stock data saved to: {processed_file}")
+
 def main():
 
     # line_graph = LineGraph(csv_file_path='./stock-data-csv-files/AAPL_processed.csv')
@@ -54,9 +77,9 @@ def main():
 
     
     # Example of how to fetch additional data and create more visualizations:
-    news = news_api.NewsData(company_name="Tesla")
-    news.fetch_news()
-    news.print_news()
+    # news = news_api.NewsData(company_name="Tesla")
+    # news.fetch_news()
+    # news.print_news()
 
     # Fetch news data
     # news_data = news.fetch_news()
